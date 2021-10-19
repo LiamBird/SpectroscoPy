39e17594437832af723ced3c48e83576df57c27d
LiamBird <43959073+LiamBird@users.noreply.github.com>
2021-10-19 18:41:41 +0100

    Update MapSelect.py
    Added docstring

--- MapSelect.py
+++ MapSelect.py
@@ -2,6 +2,34 @@ import numpy as np
 import os
 
 class MapSelect(object):
+    """
+    Interactive widget for selecting positions in Raman maps with relevant signal/ high noise to keep/ exclude from future analysis.
+    ...
+    
+    Parameters:
+    ----------
+    fname (str): 
+        EITHER previously created MapSelect file OR name for new MapSelect file
+        If previously created MapSelect file, the previously-defined Boolean values are re-loaded into the grid
+    xy_extent (tuple):
+        the (x, y) size of the grid to generate for selecting positions
+        not required if re-loading previous MapSelect file because (x, y) size is already defined
+        
+    Attributes:
+    ----------
+    grid (widget): 
+        grid of checkboxes at each (x, y) location
+        
+    Methods:
+    ----------
+    get_positions():
+        Returns Boolean array of True/ False values for the checkboxes selected by the user
+        
+    save(fname=None):
+        If fname is None, uses the fname value entered when class instantiated
+        Saves Boolean array of True/ False values to fname.npy   
+    
+    """
     def __init__(self, fname, xy_extent=None):
         import ipywidgets as wg
         
